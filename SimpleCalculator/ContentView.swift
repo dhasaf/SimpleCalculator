110e1a8d1e45c1a7ffb418eb9e7c4fadabda3296
Dhai Alassaf <dhasaf@gmail.com>
2024-03-02 23:40:09 +0300

    Initial Commit

---
+++ SimpleCalculator/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SimpleCalculator
+//
+//  Created by Dhai Alassaf on 21/08/1445 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SimpleCalculator/SimpleCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleCalculatorApp.swift
+//  SimpleCalculator
+//
+//  Created by Dhai Alassaf on 21/08/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct SimpleCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
